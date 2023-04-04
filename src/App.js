d0d998767c46882bb9f0bf834054dd28da49ce10
Galogico <galogico@gmail.com>
2023-04-04 16:15:25 -0300

    axios pegando os dados da api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { Campo, Conteudo, Fundo,Titulo } from "./styled/Layout";
 import "./public.css"
+import axios from "axios";
+import { useState,useEffect } from "react";
 function App() {
+  const [pessoas,setPessoas] = useState([]);
+  const url = "https://randomuser.me/api/?results=10"
+  useEffect(()=>{
+    axios.get(url).then(res=>console.log(res))
+  },[])
+
   return (
     <Fundo>
       <Titulo>Styled Components</Titulo>
