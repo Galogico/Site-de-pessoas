2e9706c0e5aa3138274d4fd4af0258618aa0188e
Galogico <galogico@gmail.com>
2023-04-04 15:57:54 -0300

    adicionando um titulo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Fundo,Titulo } from "./styled/Layout";
+import "./public.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fundo>
+      <Titulo>Styled Components</Titulo>
+    </Fundo>
   );
 }
 
---
+++ src/styled/Layout.js
@@ -0,0 +1,14 @@
+
+import styled from 'styled-components'
+
+export const Fundo = styled.div`
+width: 100%;
+min-height: 100vh;
+background-color: #262d37;
+`;
+
+export const Titulo = styled.div`
+color:#2da9f3;
+font-size: 32pt;
+padding: 32px 0;
+text-align: center`;
